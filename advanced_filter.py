bf378cdc67248a433af1adbb4fcbe445d1b76516
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-08-14 19:10:35 +0530

    Improved advanced_filter.py
    Improved advanced_filtering by considering exact matches now, rather than looking for instances of number only.

--- advanced_filter.py
+++ advanced_filter.py
@@ -1,21 +1,24 @@
-def advanced_filter(job_description, maximum_months_of_experience):
-    separated = job_description.split('\n')
-    matching = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    collect = []
-    for sentence in separated:
-        if 'experience' in sentence.lower():
-            for years in matching:
-                if years in sentence:
-                    if 'months' not in sentence:
-                        collect.append(int(years)*12)
-                    else:
-                        collect.append(int(years))
+import re
 
-    if len(collect) == 0 or min(collect) <= maximum_months_of_experience:
+def advanced_filter(job_description,maximum_months_of_experience):
+    lines = job_description.split('\n')
+    years = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    collected = []
+    for sentence in lines:
+        if 'experience' in sentence.lower():
+            for y in years:
+                pattern = r"\b" + re.escape(str(y)) + r"\b"
+                match = re.search(pattern, sentence)
+                if match:
+                    if 'month' not in sentence.lower():
+                        collected.append(int(match.group())*12)
+                    else:
+                        collected.append(int(match.group()))
+
+        elif 'fresher' in sentence.lower() and maximum_months_of_experience<=12:
+            return True
+    if len(collected) == 0 or min(collected)<=maximum_months_of_experience:
         return True
     else:
         return False
-
-
-
 
