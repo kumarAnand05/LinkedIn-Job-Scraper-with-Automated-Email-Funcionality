b97cafc5ad408149215a87cf81515a6baf28f9cd
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-08-14 19:23:23 +0530

    Improved User Input and Invalid input handling.
    > Improved print messages to make user input more clear.
    > Added functionality of code termination in case of invalid input.
    > Improved exception handling.

--- user_input.py
+++ user_input.py
@@ -1,11 +1,19 @@
 def role():
-    job_role = input('Enter the Job role: ')
-    return job_role.strip()
+    job_role = input('Enter the Job role (eg. Data Analyst): ')
+    if len(job_role)!=0:
+        return job_role.strip()
+    else:
+        print('No Job Role Entered. Run the script again')
+        exit()
 
 
 def geo():
     job_location = input('Enter your Job location: ')
-    return job_location.strip()
+    if len(job_location)!=0:
+        return job_location.strip()
+    else:
+        print('No Job Location provided. Run the script again')
+        exit()
 
 
 def listing_time():
@@ -14,10 +22,15 @@ def listing_time():
     for t in time_list:
         print(f'{t}:{time_list[t]}')
     job_post_time = input('Enter number to select the time of Job posting: ').strip()
-    if int(job_post_time) in range(1, 5):
-        return time_list[job_post_time]
-    else:
-        return time_list[4]
+    try:
+        if int(job_post_time) in range(1, 5):
+            return time_list[job_post_time]
+        else:
+            print("Invalid Input. Choosing default listing time, 'Any Time'.")
+            return time_list['4']
+    except ValueError:
+        print("Invalid Input. Choosing default listing time, 'Any Time'.")
+        return time_list['4']
 
 
 def job_level():
@@ -35,23 +48,35 @@ def job_level():
         print(f'{n}:{level_list[n]}')
     ex_input = input('Enter space separated number to select: ').strip().split(' ')
 
-    if len(ex_input) != 0:
-        for x in ex_input:
-            if int(x) in range(1, 8):
-                desired_exp_level.append(level_list[x])
-        return desired_exp_level
-
-    else:
+    if len(ex_input) == 0:
         print('Invalid Input. No Experience level filter applied')
         return [level_list[val] for val in level_list]
+    else:
+        for x in ex_input:
+            try:
+                if int(x) in range(1, 8):
+                    desired_exp_level.append(level_list[x])
+            except ValueError:
+                pass
+        if len(desired_exp_level)!=0:
+            return desired_exp_level
+        else:
+            print('Invalid Input. No Experience level filter applied')
+            return [level_list[val] for val in level_list]
 
 
 def list_length():
-    limit = input('\nEnter max number of Job Listings you want: ')
-    if int(limit) != 0:
-        return limit
-    else:
+    limit = input('\nEnter max number of Job Listings you want (eg. 50): ').strip()
+    try:
+        if int(limit) > 0:
+            return limit
+        else:
+            print('Invalid Input. Run the script again')
+            exit()
+    except ValueError:
         print('Invalid Input. Run the script again')
+        exit()
+
 
 
 def filtering():
@@ -63,8 +88,9 @@ def filtering():
 
 
 def internet_speed():
-    pause = input('Is your internet connection slow? (Y/N)').lower()
+    pause = input('Is your internet connection slow? (Y/N)\n').lower()
+    print('\n\nPlease Wait. Scraper is starting...')
     if pause == 'y':
-        return 2
+        return 2        # This value multiplies the pause time by this much value (Increase this value if internet is too slow, this can decrease scraping speed)
     else:
-        return 1
+        return 1        # Normal pause time, in case the internet speed is fast
