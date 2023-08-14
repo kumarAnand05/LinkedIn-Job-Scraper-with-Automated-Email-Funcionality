0729b9045bd9502e5041cd3b16f11e733be8d732
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2023-08-14 19:34:49 +0530

    Improved File Naming
    Added function to parse date used in file naming in a better and accurate way.

--- mail_system.py
+++ mail_system.py
@@ -3,12 +3,27 @@ import datetime as dt
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
+def date_suffix(today):
+    if 11 <= today <= 13:
+        suffix = 'th'
+    else:
+        last_digit = today % 10
+        if last_digit == 1:
+            suffix = 'st'
+        elif last_digit == 2:
+            suffix = 'nd'
+        elif last_digit == 3:
+            suffix = 'rd'
+        else:
+            suffix = 'th'
+    return f"{today}{suffix}"
+
 
 user_mail = 'yourmailid@goeshere.com'
 passcode = 'yourapppasscode'
 recipient = 'receivingmailid@goeshere.com'
 today = dt.date.today()
-sub = f'Job Listings {today.strftime("%d")}th {today.strftime("%b")}'
+sub = f'Job Listings {date_suffix(int(today.strftime("%d")))} {today.strftime("%b")}'
 
 
 def mail_sub(csv_file):
@@ -16,13 +31,10 @@ def mail_sub(csv_file):
     msg['From'] = user_mail
     msg['To'] = recipient
     msg['Subject'] = sub
-
     attachment = MIMEApplication(csv_file, _subtype='csv')
     attachment.add_header('Content-Disposition', 'attachment', filename=f'{sub}.csv')
     msg.attach(attachment)
     domain = user_mail.strip().split('@')[-1]
-
-    
 
     if domain == 'gmail.com':
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -31,11 +43,8 @@ def mail_sub(csv_file):
     elif domain == 'outlook.com':
         server = smtplib.SMTP('smtp-mail.outlook.com', 587)
 
-    
-
     server.starttls()
     server.login(user_mail, passcode)
-
     text = msg.as_string()
     server.sendmail(user_mail, recipient, text)
     server.quit()
